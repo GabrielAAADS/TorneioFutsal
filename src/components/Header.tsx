d584f9599b674bd03654430a594ebbad8113adf5
Matheus Gomes <79982182+Matheus9557@users.noreply.github.com>
2025-03-12 18:07:47 -0300

    Add files via upload

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return (
+      <header className="w-full h-[60px] bg-white flex justify-between items-center px-4">
+        <div className="space-y-1">
+          <div className="w-6 h-1 bg-[#515070] rounded"></div>
+          <div className="w-6 h-1 bg-[#515070] rounded"></div>
+          <div className="w-6 h-1 bg-[#515070] rounded"></div>
+        </div>
+        <div className="w-[34px] h-[34px] bg-[#515070] flex items-center justify-center">
+          <div className="w-[17.67px] h-[16.42px] bg-white"></div>
+        </div>
+      </header>
+    );
+  }
+  
